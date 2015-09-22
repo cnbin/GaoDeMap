5a605b71e784f695e491855d0d9ac665ed62960b
binbin <845040571@qq.com>
2015-09-22 18:10:42 +0800

    first commit

---
+++ GaoDeDiTu/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GaoDeDiTu
+//
+//  Created by Apple on 9/22/15.
+//  Copyright © 2015 binbin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GaoDeDiTu/ViewController.h
@@ -0,0 +1,16 @@
+//
+//  ViewController.h
+//  GaoDeDiTu
+//
+//  Created by Apple on 9/22/15.
+//  Copyright © 2015 binbin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface ViewController : UIViewController
+
+
+@end
+
